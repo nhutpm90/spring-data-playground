1fda675be00202742cd49f4780cf678c8943d1f3
nhut_pham <nhut.pham@ilogic.vn>
2022-06-30 16:20:13 +0700

    add filter api

--- src/main/java/com/springdata/demo/api/EmployeeApi.java
+++ src/main/java/com/springdata/demo/api/EmployeeApi.java
@@ -1,9 +1,10 @@
 package com.springdata.demo.api;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springdata.demo.entity.Employee;
@@ -16,7 +17,21 @@ public class EmployeeApi {
 	private EmployeeService employeeService;
 	
 	@GetMapping("/employees")
-	public List<Employee> employees() throws Exception {
-		return this.employeeService.employees();
+	public Page<Employee> employees(
+			@RequestParam(defaultValue = "id") String sortBy,
+			@RequestParam(defaultValue = "ASC") Sort.Direction sortDirection,
+			@RequestParam(defaultValue = "0") int page, 
+			@RequestParam(defaultValue = "10") int size) throws Exception {
+		return this.employeeService.employees(sortBy, sortDirection, page, size);
+	}
+	
+	@GetMapping("/employees/filter")
+	public Page<Employee> filterEmployees(
+			@RequestParam(required=false) String gender,
+			@RequestParam(defaultValue = "id") String sortBy,
+			@RequestParam(defaultValue = "ASC") Sort.Direction sortDirection,
+			@RequestParam(defaultValue = "0") int page, 
+			@RequestParam(defaultValue = "10") int size) throws Exception {
+		return this.employeeService.filterEmployees(gender, sortBy, sortDirection, page, size);
 	}
 }
--- src/main/java/com/springdata/demo/service/EmployeeService.java
+++ src/main/java/com/springdata/demo/service/EmployeeService.java
@@ -1,9 +1,22 @@
 package com.springdata.demo.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.springdata.demo.entity.Employee;
 import com.springdata.demo.repo.EmployeeRepo;
@@ -11,10 +24,54 @@ import com.springdata.demo.repo.EmployeeRepo;
 @Service
 public class EmployeeService {
 
+	@PersistenceContext
+	private EntityManager em;
+	
 	@Autowired
 	private EmployeeRepo employeeRepo;
-	
-	public List<Employee> employees() {
-		return this.employeeRepo.findAll();
+
+	public Page<Employee> employees(String sortBy, Sort.Direction sortDirection, int page, int size) {
+		Sort sort = Sort.by(sortDirection, sortBy);
+		PageRequest pageRequest = PageRequest.of(page, size, sort);
+		return this.employeeRepo.findAll(pageRequest);
 	}
+	
+	public Page<Employee> filterEmployees(String gender, String sortBy, Sort.Direction sortDirection, int page, int size) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Employee> query = cb.createQuery(Employee.class);
+		Root<Employee> root = query.from(Employee.class);
+		
+		List<Predicate> conditions = new ArrayList<Predicate>();
+		if (StringUtils.hasLength(gender)) {
+			conditions.add(cb.equal(root.get("gender"), gender));
+		}
+		
+		Predicate predicate = cb.and(conditions.toArray(new Predicate[0]));
+		
+		query.where(predicate);
+		
+		if(sortDirection.equals(Sort.Direction.ASC)) {
+			query.orderBy(cb.asc(root.get(sortBy)));
+		} else {
+			query.orderBy(cb.desc(root.get(sortBy)));
+		}
+		
+		List<Employee> employees = em.createQuery(query)
+				.setFirstResult(page * size).setMaxResults(size)
+				.getResultList();
+		
+		Sort sort = Sort.by(sortDirection, sortBy);
+		PageRequest pageRequest = PageRequest.of(page, size, sort);
+		Long count = this.countEmployee(predicate);
+		
+		return new PageImpl<>(employees, pageRequest, count);
+	}
+	
+	private Long countEmployee(Predicate conditions) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Long> query = cb.createQuery(Long.class);
+		Root<Employee> root = query.from(Employee.class);
+		return em.createQuery(query.select(cb.count(root)).where(conditions)).getSingleResult();
+	}
+	
 }
